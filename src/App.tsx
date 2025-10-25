2133e668d045adb1a42465532055160140a9360a
OGEA\cristiano.souza <cristiano.souza@ogea.com.br>
2025-10-24 22:49:32 -0300

    projeto chronos pomodoro

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,16 @@
-import "./styles/theme.css";
-import "./styles/global.css";
+import { TaskContextProvider } from './contexts/TaskContext/TaskContextProvider';
+import { MessagesContainer } from './components/MessagesContainer';
+import { MainRouter } from './routers/MainRouter';
+
+import './styles/theme.css';
+import './styles/global.css';
 
 export function App() {
   return (
-    <>
-      <div className="container">
-        <div className="content">
-          <section>LOGO</section>
-        </div>
-      </div>
-
-      <div className="container">
-        <div className="content">
-          <section>MENU</section>
-        </div>
-      </div>
-
-      <div className="container">
-        <div className="content">
-          <section>FORM</section>
-        </div>
-      </div>
-
-      <div className="container">
-        <div className="content">
-          <section>FOOTER</section>
-        </div>
-      </div>
-    </>
+    <TaskContextProvider>
+      <MessagesContainer>
+        <MainRouter />
+      </MessagesContainer>
+    </TaskContextProvider>
   );
 }
